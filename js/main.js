3caf6c7bc97d1f489a11cc00c318a67cf17be092
mdabdulkyum1 <kyummdabdul@gmail.com>
2024-10-10 22:50:35 +0600

    clear project done

--- js/main.js
+++ js/main.js
@@ -1,8 +1,17 @@
 const loadUsers = async () => {
-    const res = await fetch('https://forbes400.onrender.com/api/forbes400/');
+    const res = await fetch('https://forbes400.onrender.com/api/forbes400?limit=10');
     const data = await res.json();
-    
-    displayUsers(data);
+    renderRandomUsers(data);
+}
+
+const showLoading = () => {
+    const loading = document.getElementById('loading');
+    const userDataContainer = document.getElementById('user-data-container');
+    loading.classList.remove('hidden')
+    setTimeout(() => {
+        loading.classList.add('hidden')
+        userDataContainer.classList.remove('hidden')
+    }, 500);
 }
 
 loadUsers();
--- js/utilites.js
+++ js/utilites.js
@@ -1,279 +1,39 @@
-// const displayUsers = (data) => {
-//     addUserRandomly(data)
-//     loading();
-// }
-// // add-user-randomly 
-// const addUserRandomly = (data) => {
+let randomUsers = [];
+const renderRandomUsers = (users) => {
+    document.getElementById('add-user-randomly').addEventListener('click', () => {
 
-//     const allUsers = [];
+        const randomValue = getRandomValue(users.length);
 
-//     document.getElementById('add-user-randomly')
-//         .addEventListener('click', () => {
-
-
-//             const randomDigit = randomValue(data.length)
-
-//             const userExists = allUsers.find((a) => a.person.name === data[randomDigit].person.name);
-//             userExists ? `` : allUsers.push(data[randomDigit]);
-
-//             const userDataContainer = document.getElementById('user-data-container');
-//             userDataContainer.innerHTML = "";
-//             allUsers.forEach(users => {
-//                 const {
-//                     person,
-//                     countryOfCitizenship,
-//                     industries,
-//                     rank,
-//                     finalWorth
-//                 } = users;
-//                 calculateWealth(finalWorth);
-//                 const tr = document.createElement('tr');
-//                 tr.innerHTML = `
+        const existsUsers = randomUsers.find(user => user.person.name === users[randomValue].person.name);
+        existsUsers ? alert("no") : randomUsers.push(users[randomValue]) ;
         
-//         <td class="p-4 flex items-center">
-//            ${person.name}                 
-//            <i class="fas fa-eye ml-2 text-blue-600"></i>
-//         </td>
-//         <td class="p-4">${countryOfCitizenship}</td>
-//         <td class="p-4">${industries[0]}</td>
-//         <td class="p-4">${rank}</td>
-//         <td class="p-4">$${finalWorth}</td>
-//     `;
-//                 userDataContainer.append(tr);
-//             });
+        displayUsers(randomUsers)
+        showLoading();
+    });
 
 
-//         });
 
-// }
+}
 
-// //Calculate Entire Wealth
-// const arrayWorth = [];
-// const calculateWealth = (worth) => {    
-//     arrayWorth.push(worth);
-//     const totalWorth = arrayWorth.reduce((acc, curr) => acc + curr, 0);
-//     console.log(arrayWorth)
-//     console.log("totoal is", totalWorth)
-// }
+// generate random number array
+const getRandomValue = (length) => {
+    return Math.floor(Math.random() * length)
+}
 
 
+/// display users
+const displayUsers = (users) => {
 
-// // generate random value
-// const randomValue = (length) => {
-//     return Math.floor(Math.random() * length);
-
-// }
-
-// // show loading
-// const loading = () => {
-//     const loading = document.getElementById("loading");
-//     loading.classList.remove('hidden');
-//     // loading off
-//     setTimeout(() => {
-//         loading.classList.add('hidden');
-//     }, 2000)
-
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const displayUsers = (data) => {
-//     addUserRandomly(data);
-// };
-
-// // Add user randomly
-// const addUserRandomly = (data) => {
-//   const allUsers = [];
-
-//   document.getElementById('add-user-randomly').addEventListener('click', () => {
-//     const randomIndex = randomValue(data.length); // Generate a random index
-//     const randomUser = data[randomIndex]; // Select random user
-
-//     // Check if the user is already added by comparing a property (e.g., person.name)
-//     const userExists = allUsers.some(user => user.person.name === randomUser.person.name);
-
-//     if (userExists) {
-//       alert('User already added, please try another one!');
-//     } else {
-//       allUsers.push(randomUser); // Add the new random user to the array
-
-//       const userDataContainer = document.getElementById('user-data-container');
-
-//       // Destructure randomUser data
-//       const { person, countryOfCitizenship, industries, rank, finalWorth } = randomUser;
-
-//       // Create a new row for the random user
-//       const tr = document.createElement('tr');
-//       tr.innerHTML = `
-//         <td class="p-4 flex items-center">
-//           ${person.name}
-//           <i class="fas fa-eye ml-2 text-blue-600"></i>
-//         </td>
-//         <td class="p-4">${countryOfCitizenship}</td>
-//         <td class="p-4">${industries[0]}</td>
-//         <td class="p-4">${rank}</td>
-//         <td class="p-4">$${finalWorth}</td>
-//       `;
-//       userDataContainer.append(tr);
-
-//       console.log(allUsers); // Log the list of users
-//       loading(); // Show loading spinner
-//     }
-//   });
-// };
-
-// // Generate random value
-// const randomValue = (length) => {
-//   return Math.floor(Math.random() * length); // Use length to generate a valid index
-// };
-
-// // Show loading
-// const loading = () => {
-//   const loading = document.getElementById('loading');
-//   loading.classList.remove('hidden');
-
-//   // Hide loading after 2 seconds
-//   setTimeout(() => {
-//     loading.classList.add('hidden');
-//   }, 2000);
-// };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Persistent variables to store wealth
-let totalWorth = 0;
-const allUsers = [];
-
-const displayUsers = (data) => {
-    addUserRandomly(data);
-    loading();
-};
-
-// Add user randomly
-const addUserRandomly = (data) => {
-    document.getElementById('add-user-randomly')
-        .addEventListener('click', () => {
-            document.getElementById('totalWorth').innerText = `$${totalWorth.toFixed(3)}`;
-            const randomDigit = randomValue(data.length);
-            
-            // Check if user already exists in the list
-            const userExists = allUsers.find((a) => a.person.name === data[randomDigit].person.name);
-            if (!userExists) {
-                // Add the new user and update total wealth
-                allUsers.push(data[randomDigit]);
-                calculateWealth(data[randomDigit].finalWorth);
-            }
-
-            // Render the updated user list
-            renderUsers(allUsers);
-        });
-};
-
-// Calculate total wealth
-const calculateWealth = (worth) => {
-    totalWorth += worth; // Add new wealth to total
-    console.log("Total wealth is:", totalWorth); // Display updated total
-};
-
-// Render users to the table
-const renderUsers = (users) => {
     const userDataContainer = document.getElementById('user-data-container');
-    userDataContainer.innerHTML = ""; // Clear current users
-
+    userDataContainer.innerHTML = "";
     users.forEach(user => {
-        const { person, countryOfCitizenship, industries, rank, finalWorth } = user;
-
-        // Create table row for user
+        const {
+            person,
+            countryOfCitizenship,
+            industries,
+            rank,
+            finalWorth
+        } = user;
         const tr = document.createElement('tr');
         tr.innerHTML = `
             <td class="p-4 flex items-center">
@@ -287,19 +47,5 @@ const renderUsers = (users) => {
         `;
         userDataContainer.append(tr);
     });
-};
 
-// Generate random value
-const randomValue = (length) => {
-    return Math.floor(Math.random() * length);
-};
-
-// Show loading
-const loading = () => {
-    const loading = document.getElementById("loading");
-    loading.classList.remove('hidden');
-    // loading off
-    setTimeout(() => {
-        loading.classList.add('hidden');
-    }, 2000);
-};
+}
